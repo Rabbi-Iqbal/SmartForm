6af8db4aa09d5fa16476eec70b0c7e8a17e2a9e9
Rabbi Iqbal <rabbi425@gmail.com>
2018-02-12 09:17:23 +0600

    getting started with jQuery

--- smartForm.js
+++ smartForm.js
@@ -1,3 +1,31 @@
 // Your jQuery goes here
 
-var userData = {};
+var userData = {
+  name:'',
+  email:''
+  html: {likes[], dislikes: []},
+  css: {likes[], dislikes: []},
+  js: {likes[], dislikes: []},
+  strength: {css: '', js: '', html: ''},
+  currentQuestion: '#welcome'
+};
+
+ $('#start').click(function(){
+   $('#welcome').hide();
+   $('#q1').show();
+ });
+
+ $('#name').change(function(event){
+   console.log($('#name').val());
+ });
+
+ $('#q1next').click(function(){
+   if($('#name').val() && $('#email').val()){
+   userData.name= $('#name').val();
+   userData.email= $('#email').val();
+   $('#q2').show();
+   $('#q1').hide();
+ }else{
+   alert('Please enter user name and email')
+ }
+ });
